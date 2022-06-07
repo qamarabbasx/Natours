61d43d78e4866632dd71206ebd1cfc89a46540b5
Qamar Abbas <reply.qamar@gmail.com>
2022-06-08 00:13:27 +0500

    Get method(read) on local api

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
-app.get('/', (req, res) => {
-  res.status(200).json({ Hello: 'Hello from the server', ID: 56 });
-});
-app.post('/', (req, res) => {
-  res.end('You can post to this endpoint');
+// app.get('/', (req, res) => {
+//   res.status(200).json({ Hello: 'Hello from the server', ID: 56 });
+// });
+// app.post('/', (req, res) => {
+//   res.end('You can post to this endpoint');
+// });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 const port = 3000;
 app.listen(port, () => {
