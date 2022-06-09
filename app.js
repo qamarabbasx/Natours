908ccc54a6ad17a9e992d8eb0dec2740da431d9c
Qamar Abbas <reply.qamar@gmail.com>
2022-06-09 22:37:49 +0500

    Refactored my code , removed comments, functions seperated

--- app.js
+++ app.js
@@ -2,19 +2,11 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
-// middleware that can modify the incomming requests
-// middleware between req and response
 app.use(express.json());
-// app.get('/', (req, res) => {
-//   res.status(200).json({ Hello: 'Hello from the server', ID: 56 });
-// });
-// app.post('/', (req, res) => {
-//   res.end('You can post to this endpoint');
-// });
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -22,19 +14,11 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
-});
-app.get('/api/v1/tours/:id', (req, res) => {
-  // req.params stores all the parameters which we define in the API
+};
+const getTour = (req, res) => {
   console.log(req.params);
   // *1 is the trick to change the string to a NUMBER
   const id = req.params.id * 1;
-  // First solution to handle if we want to get ID which is not defined in our DB
-  // if (id >= tours.length) {
-  //   return res.status(404).json({
-  //     status: 'fail',
-  //     message: 'Invalid ID',
-  //   });
-  // }
   const tour = tours.find((el) => el.id === id);
   // Second solution to handle if we want to get ID which is not defined in our DB
   if (!tour) {
@@ -49,8 +33,8 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour,
     },
   });
-});
-app.post('/api/v1/tours', (req, res) => {
+};
+const createTour = (req, res) => {
   // console.log(req.body);
   const newID = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newID }, req.body);
@@ -67,9 +51,8 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
-// we are not updating  the data here because its hude javascript --> we have to read json file then read properties then update that data, this is dummy data thats why we are not doing update with this dummy json file, BECAUSE in real world we deal with databases not json filess
-app.patch('/api/v1/tours/:id', (req, res) => {
+};
+const updateTour = (req, res) => {
   if (req.params.id * 1 >= tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -83,8 +66,8 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: '<Updated Tour Here...>',
     },
   });
-});
-app.delete('/api/v1/tours/:id', (req, res) => {
+};
+const deleteTour = (req, res) => {
   if (req.params.id * 1 >= tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -96,7 +79,18 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: null,
   });
-});
+};
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on ${port}...`);
