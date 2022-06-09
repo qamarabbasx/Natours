2a16df011d5d624bea8ef5409392ec720aeb4189
Qamar Abbas <reply.qamar@gmail.com>
2022-06-09 19:30:11 +0500

    Handled Delete  Requests

--- app.js
+++ app.js
@@ -84,6 +84,19 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     },
   });
 });
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 >= tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  // status code 204 means no content
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on ${port}...`);
