bb0d40f264d3f4ee78a9ca71c21b363950085f45
Qamar Abbas <reply.qamar@gmail.com>
2022-06-10 19:24:04 +0500

    A Better File structure: Separetd Routes and route handlers in separete files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 // 1) Middleware
@@ -16,128 +17,10 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-// 2) Route Handlers
-const getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requestedAT: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
-};
-const getTour = (req, res) => {
-  console.log(req.params);
-  // *1 is the trick to change the string to a NUMBER
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-  // Second solution to handle if we want to get ID which is not defined in our DB
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-};
-const createTour = (req, res) => {
-  // console.log(req.body);
-  const newID = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newID }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-};
-const updateTour = (req, res) => {
-  if (req.params.id * 1 >= tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated Tour Here...>',
-    },
-  });
-};
-const deleteTour = (req, res) => {
-  if (req.params.id * 1 >= tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  // status code 204 means no content
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-};
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
 // 3) Routes
-const tourRouter = express.Router();
-const userRouter = express.Router();
-tourRouter.route('/').get(getAllTours).post(createTour);
-tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
-userRouter.route('/').get(getAllUsers).post(createUser);
-userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-// 4) Start the server
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on ${port}...`);
-});
+
+module.exports = app;
---
+++ controllers/tourController.js
@@ -0,0 +1,80 @@
+const fs = require('fs');
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+exports.getAllTours = (req, res) => {
+  console.log(req.requestTime);
+  res.status(200).json({
+    status: 'success',
+    requestedAT: req.requestTime,
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+exports.getTour = (req, res) => {
+  console.log(req.params);
+  // *1 is the trick to change the string to a NUMBER
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  // Second solution to handle if we want to get ID which is not defined in our DB
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+exports.createTour = (req, res) => {
+  // console.log(req.body);
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newID }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+exports.updateTour = (req, res) => {
+  if (req.params.id * 1 >= tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated Tour Here...>',
+    },
+  });
+};
+exports.deleteTour = (req, res) => {
+  if (req.params.id * 1 >= tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  // status code 204 means no content
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
