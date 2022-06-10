f6fcd6967abf120093ddb4613d19f6358e845938
Qamar Abbas <reply.qamar@gmail.com>
2022-06-10 23:20:23 +0500

    Eslint and prettier configuration

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+
 // console.log(process.env);
 const port = process.env.PORT;
 app.listen(port, () => {
