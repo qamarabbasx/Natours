8ac6321287e6b3331a5d05a0c645f42bef6c4311
Qamar Abbas <reply.qamar@gmail.com>
2022-06-17 15:07:04 +0500

    CRUD: Created documents using schema models

--- server.js
+++ server.js
@@ -15,6 +15,32 @@ mongoose
     useFindAndModify: false,
   })
   .then(() => console.log('Connection to database SUCCESSFUL'));
+const tourSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Tour Must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Tour Must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => console.log('Error🔥: ', err));
 
 // console.log(process.env);
 const port = process.env.PORT;
