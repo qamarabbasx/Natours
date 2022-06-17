3c30f693fdf17228ab7266ca87ed518d7935cf5e
Qamar Abbas <reply.qamar@gmail.com>
2022-06-17 23:35:23 +0500

    CRUD: Delete Documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -72,10 +72,18 @@ exports.updateTour = async (req, res) => {
     });
   }
 };
-exports.deleteTour = (req, res) => {
+exports.deleteTour = async (req, res) => {
   // status code 204 means no content
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
