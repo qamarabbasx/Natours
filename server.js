e639fa3a0af1e125dea97406476f1e6000c354d1
Qamar Abbas <reply.qamar@gmail.com>
2022-06-17 13:02:07 +0500

    Connection of DB with Node App

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  // .connect(process.env.DATABASE_LOCAL,{
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('Connection to database SUCCESSFUL'));
 
 // console.log(process.env);
 const port = process.env.PORT;
