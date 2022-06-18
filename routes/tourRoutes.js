da2495755eec5770abb79a160a166079d0cf31ec
Qamar Abbas <reply.qamar@gmail.com>
2022-06-19 00:00:37 +0500

    Aggregation Pipeline

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
+router.route('/tour-stats').all(tourController.getTourStats);
 router
   .route('/')
   .get(tourController.getAllTours)
