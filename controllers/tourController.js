543ed247e5ec932373831d34a077debf32425c77
Qamar Abbas <reply.qamar@gmail.com>
2022-06-18 17:17:51 +0500

    Sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -11,7 +11,7 @@ exports.getAllTours = async (req, res) => {
     // 1 B) Advance Filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
+    // console.log(JSON.parse(queryStr));
     let query = Tour.find(JSON.parse(queryStr));
     // 2)Sorting
     if (req.query.sort) {
