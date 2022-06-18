b199268efc37c44d771c82bee8dad6f2176164fe
Qamar Abbas <reply.qamar@gmail.com>
2022-06-18 09:19:36 +0500

    Completed Model Scema

--- models/tourModels.js
+++ models/tourModels.js
@@ -5,15 +5,52 @@ const tourSchema = mongoose.Schema({
     type: String,
     required: [true, 'Tour Must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'Tour Must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'Tour must have a summary description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
