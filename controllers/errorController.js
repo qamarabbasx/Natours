1d132caacc2aada6d9f2a708ad05af2ebb2fcabd
Qamar Abbas <reply.qamar@gmail.com>
2022-06-21 08:36:55 +0500

    Error Handling: errors during development and production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,36 @@
-module.exports = (err, req, res, next) => {
-  //   console.log(err.stack);
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'fail';
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
+    error: err,
+    stack: err.stack,
   });
+};
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message tothe client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Programming or other unknown error: don't leak error details
+  } else {
+    // 1) log error
+    console.log('Error 🔥: ', err);
+    // 2) send generic mesage
+    res.status(500).json({
+      status: 'error',
+      message: 'something went very wrong',
+    });
+  }
+};
+module.exports = (err, req, res, next) => {
+  //   console.log(err.stack);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
