b230d3a47f81d35ef5ca4283d5f9567514c2535a
Qamar Abbas <reply.qamar@gmail.com>
2022-06-21 18:34:58 +0500

    Error Handling: Uncaught Exceptions outside express

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('Uncaught Exception  🔥 Shutting Down');
+  process.exit(1); // 1 is for uncaught exception
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
