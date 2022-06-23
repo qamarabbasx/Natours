28bf9cfee9532e1254d8d17f7e95c55f39e24418
Qamar Abbas <reply.qamar@gmail.com>
2022-06-23 12:19:43 +0500

    password validation and password encryption

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema(
   {
@@ -25,6 +26,13 @@ const userSchema = mongoose.Schema(
     confirmPassword: {
       type: String,
       required: [true, 'Please confirm your password'],
+      validate: {
+        // This validator only works on Create and Save !!!
+        validator: function (el) {
+          return el === this.password;
+        },
+        message: 'Passwords are not the same !',
+      },
     },
   },
   {
@@ -32,5 +40,16 @@ const userSchema = mongoose.Schema(
     toObject: { virtuals: true },
   }
 );
+userSchema.pre('save', async function (next) {
+  // Only runs this function if the password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12 // cost is cpu intensive greater the value more it will be cpu intensive
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete confirmPassword feild from database to persist
+  this.confirmPassword = undefined;
+  next();
+});
 const User = mongoose.model('User', userSchema);
 module.exports = User;
