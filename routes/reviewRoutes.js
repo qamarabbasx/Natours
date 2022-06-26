a5512cb789e872190c2e65bb1b0f718c0a2670e0
Qamar Abbas <reply.qamar@gmail.com>
2022-06-27 00:59:15 +0500

    Added Missing Authentication and Authorization i All 3 Routes

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,15 +4,11 @@ const authController = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
-// POST /tour/4273542/reviews
-// GET /tour/4273542/reviews
-// POST /reviews
-
+router.use(authController.protect);
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview
@@ -20,10 +16,12 @@ router
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
+  .patch(
+    authController.restrictTo('admin', 'user'),
+    reviewController.updateReview
+  )
   .delete(
-    authController.protect,
-    authController.restrictTo('admin', 'lead-guide'),
+    authController.restrictTo('admin', 'user'),
     reviewController.deleteReview
   );
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,19 +8,16 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
+// Protect All after this middleware (becasue of synchronous behavior of JS)
+router.use(authController.protect);
+router.patch('/updateMyPassword', authController.updatePassword);
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+
+// Restrict All except admin after this middleware
+router.use(authController.restrictTo('admin'));
 router
   .route('/')
   .get(userController.getAllUsers)
