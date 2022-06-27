6fbf8457a0577666cff882a178fda362db7716a5
Qamar Abbas <reply.qamar@gmail.com>
2022-06-27 16:36:03 +0500

    Calculated Average rating on Tours

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const { findOne } = require('./tourModel');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -44,6 +46,48 @@ reviewSchema.pre(/^find/, function (next) {
     select: 'name photo',
   });
   next();
+});
+
+// static Function to calculate average ratings and number of ratings
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRatings: { $sum: 1 },
+        avgRatings: { $avg: '$rating' },
+      },
+    },
+  ]);
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsAverage: stats[0].avgRatings,
+      ratingsQuantity: stats[0].nRatings,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsAverage: 4.5,
+      ratingsQuantity: 0,
+    });
+  }
+};
+// Using POST middleware to call the above function
+reviewSchema.post('save', function () {
+  this.constructor.calcAverageRatings(this.tour);
+});
+// 2 STEP process to call above function for (update or delete reviews)
+// Step-1 finding the query and saving into this variable to make it accessible for other middlewares
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.findOne();
+  next();
+});
+// step-2 called the above function to make changes
+reviewSchema.post(/^findOneAnd/, async function () {
+  // this.r = await this.findOne(); doesn't work here, query has already executed
+  await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
