c47d9a7bebadf5b8938f9790c203dcc649baf2fe
Qamar Abbas <reply.qamar@gmail.com>
2022-06-28 23:36:38 +0500

    Built Tour overview page with PUG and loved doing that ♥

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,14 @@
-exports.getOverview = (req, res) => {
+// const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+const Tour = require('../models/tourModel');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  const tours = await Tour.find();
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
