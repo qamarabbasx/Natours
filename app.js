4e1211bdcc8dd1c23a1c274fc08023d59b4229cb
Qamar Abbas <reply.qamar@gmail.com>
2022-07-04 03:24:27 +0500

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -22,7 +23,19 @@ const app = express();
 app.enable('trust proxy');
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+
 // 1) Global Middleware
+
+// Implement cors
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: 'https://www.natours.com',
+//   })
+// );
+app.options('*', cors());
+// app.options('/api/v1/tours/:tourId', cors());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
