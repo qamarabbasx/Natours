a71bae8d4ad7c15f17506299550aa71595ed0159
Qamar Abbas <reply.qamar@gmail.com>
2022-07-04 21:10:06 +0500

    Improved stripe implementation 2

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -49,12 +49,10 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckout = async (session) => {
-  console.log('Im in create booking checkout');
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
-  console.log(tour, user, price);
 };
 exports.webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature'];
