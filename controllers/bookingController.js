cd3e753be4430470eb8606a47ef02c3978ee29d6
Qamar Abbas <reply.qamar@gmail.com>
2022-07-04 23:27:58 +0500

    Improved stripe implementation Final

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -57,6 +57,7 @@ const createBookingCheckout = async (session) => {
 };
 exports.webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature'];
+  console.log('webhook checkout ', signature);
   let event;
   try {
     event = stripe.webhooks.constructEvent(
