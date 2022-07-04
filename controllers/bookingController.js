4535b8abfbced452b738e732420f0994b2a64068
Qamar Abbas <reply.qamar@gmail.com>
2022-07-04 21:01:43 +0500

    Improved stripe implementation 2

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -51,8 +51,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   console.log('Im in create booking checkout');
   const tour = session.client_reference_id;
-  const user = await User.findOne({ email: session.customer_email }).id;
-  const price = session.line_items[0].amount / 100;
+  const user = (await User.findOne({ email: session.customer_email })).id;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
   console.log(tour, user, price);
 };
@@ -70,7 +70,6 @@ exports.webhookCheckout = (req, res, next) => {
   }
   if (event === 'checkout-session-completed')
     createBookingCheckout(event.data.object);
-  console.log(createBookingCheckout(event.data.object));
 
   res.status(200).json({ received: true });
 };
